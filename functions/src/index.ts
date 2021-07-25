d1edf082f170166c48d169ac2ceb9fb81e4a241a
将之大西 <omccello@gmail.com>
2021-07-25 12:35:36 +0900

    fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,30 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { User } from './types/User';
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+admin.initializeApp();
+
+export const onUpdateUser = functions
+  .region('asia-northeast1')
+  .firestore.document('users/{userUd}')
+  .onUpdate(async (change, context) => {
+    const { userId } = context.params;
+    const newUser = change.after.data() as User;
+    const db = admin.firestore();
+
+    try {
+      const snapshot = await db
+        .collectionGroup('reviews')
+        .where('user.id', '==', userId)
+        .get();
+
+      const batch = db.batch();
+      snapshot.docs.forEach((reviewDoc) => {
+        const user = { ...reviewDoc.data().user, name: newUser.name };
+        batch.update(reviewDoc.ref, { user });
+      });
+      await batch.commit();
+    } catch (error) {
+      console.error(error);
+    }
+  });
